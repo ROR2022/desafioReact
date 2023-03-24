194f9c88ccbc96a1fba03a247e7e6a910ddc4d4e
LaraGutz_Mac <laragutz@gmail.com>
2023-03-23 19:19:46 -0600

    realizando cambios intermedio

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useDevto from '../hooks/useDevto';
 import { Link } from 'react-router-dom';
+import { Table, TableRow, TableBody, TableContainer, Container } from '@mui/material'
 
 const ArticlePage = () => {
     const [dataPosts]=useDevto();
@@ -10,6 +11,18 @@ const ArticlePage = () => {
             <Link to={'/'}>HOME</Link>
             <br />
             <h1>Article Page</h1>
+            <TableContainer>
+            <Table>
+                <TableBody>
+                    {dataPosts.map((post)=>(
+                        <TableRow>
+                            <td>{post.title}</td>
+                            <td>prueba</td>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
         </div>
     )
 }
