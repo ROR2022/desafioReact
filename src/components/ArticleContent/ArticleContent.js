76fb021eda2356de6e5916f0e32eb14ddbb1f53c
LaraGutz_Mac <laragutz@gmail.com>
2023-03-24 18:36:15 -0600

    entendiendo

--- src/components/ArticleContent/ArticleContent.js
+++ src/components/ArticleContent/ArticleContent.js
@@ -3,21 +3,23 @@ import React from 'react'
 import MainAsideArticle from './MainAsideArticle/MainAsideArticle'
 import MainContentArticle from './MainContentArticle/MainContentArticle'
 import MongoAsideArticle from './MongoAsideArticle/MongoAsideArticle'
-//import './ArticleContent.scss'
+
 
 const Article = () => {
   return (
-    <Box id="mainBox">
-      <Box>
-        <MainAsideArticle/>
+    <div>
+      <Box id="mainBox">
+        <Box>
+          <MainAsideArticle/>
+        </Box>
+        <Box>
+          <MainContentArticle/>
+        </Box>
+        <Box>
+          <MongoAsideArticle/>
+        </Box>
       </Box>
-      <Box>
-        <MainContentArticle/>
-      </Box>
-      <Box>
-        <MongoAsideArticle/>
-      </Box>
-    </Box>
+    </div>
   )
 }
 
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,16 +2,19 @@ import React from 'react'
 import useDevto from '../hooks/useDevto';
 import { Link } from 'react-router-dom';
 import ArticleContent from '../components/ArticleContent/ArticleContent';
-
+import DevtoNavbar from "../components/DevtoNavBar/DevtoNavbar";
+import DevtoFooter from "../components/DevtoFooter/DevtoFooter";
 
 const ArticlePage = () => {
     const [dataPosts]=useDevto();
     console.log('dataPosts:..',dataPosts);  
     return (
         <div>
+            <DevtoNavbar/>
             <Link to={'/'}>HOME</Link>
             <br />
             <ArticleContent/>
+            <DevtoFooter/>
         </div>
     )
 }
